701d886a02a38a33d265f4dcb0665cc366631917
--replace-all <jaime.jim8@gmail.com>
2021-11-10 18:36:14 +0100

    Todo hecho

--- funciones.js
+++ funciones.js
@@ -8,6 +8,8 @@ function dados(num_dados){
 var eleccion;
 var num_uno;
 var num_dos;
+var porcentaje_aciertos= new Array();
+var i=0;
 
 function mates(){
     document.getElementById("resultados_mates").innerHTML="<h3>Introduzca la repuesta:</h3>";
@@ -54,72 +56,85 @@ function corregir(){
     var resultado_jugador=document.getElementById("resultado_operaciones_jugador").value;
     var escribir=document.getElementById("respuesta");
     var boton_corregir=document.getElementById("boton_corregir_mates");
+    var boton_porcentajes=document.getElementById("boton_porcentaje");
     var boton_empezar=document.getElementById("boton_empezar_mates");//Les asignamos una variable debido a que como le vamos a llamar muchas veces, por si acaso ocurre un error o un cambio, podemos cambiar mas sencillamente
+    var correcto=false;
     switch(eleccion){
         case 1:
             if(num_uno+num_dos==resultado_jugador){
-                escribir.innerHTML="<h4>Correcto</h4>  <h5>¿Desea jugar de nuevo?</h5>";
-                boton_empezar.style.display="block";
-                boton_corregir.style.display="none";
+                correcto=true;
+
             }
             else{
-                escribir.innerHTML="<h4 id='letra_incorrecto'>Incorrecto</h4>  <h5>Vuelva a intentarlo</h5>";
+                correcto=false;
             }
             break;
         case 2:
             if(num_uno*num_dos==resultado_jugador){
-                escribir.innerHTML="<h4>Correcto</h4>  <h5>¿Desea jugar de nuevo?</h5>";
-                boton_empezar.style.display="block";
-                boton_corregir.style.display="none";
+                correcto=true;
             }
             else{
-                escribir.innerHTML="<h4 id='letra_incorrecto'>Incorrecto</h4>  <h5>Vuelva a intentarlo</h5>";
+                correcto=false;
             }
             break;
         case 3:
             if(num_uno>num_dos){
                 if(num_uno-num_dos==resultado_jugador){
-                    escribir.innerHTML="<h4>Correcto</h4>  <h5>¿Desea jugar de nuevo?</h5>";
-                    boton_empezar.style.display="block";
-                    boton_corregir.style.display="none";
+                    correcto=true;
                 }
                 else{
-                    escribir.innerHTML="<h4 id='letra_incorrecto'>Incorrecto</h4>  <h5>Vuelva a intentarlo</h5>";
+                    correcto=false;
                 }
             }
             else{
                 if(num_dos-num_uno==resultado_jugador){
-                    escribir.innerHTML="<h4>Correcto</h4>  <h5>¿Desea jugar de nuevo?</h5>";
-                    boton_empezar.style.display="block";
-                    boton_corregir.style.display="none";
+                    correcto=true;
                 }
                 else{
-                    escribir.innerHTML="<h4 id='letra_incorrecto'>Incorrecto</h4>  <h5>Vuelva a intentarlo</h5>";
+                    correcto=false;
                 }
             }
             break;
         case 4:
             if(num_uno>num_dos){
                 if(num_uno/num_dos==resultado_jugador){
-                    escribir.innerHTML="<h4>Correcto</h4>  <h5>¿Desea jugar de nuevo?</h5>";
-                    boton_empezar.style.display="block";
-                    boton_corregir.style.display="none";
+                    correcto=true;
                 }
                 else{
-                    escribir.innerHTML="<h4 id='letra_incorrecto'>Incorrecto</h4>  <h5>Vuelva a intentarlo</h5>";
+                    correcto=false;
                 }
             }
             else{
                 if(num_dos/num_uno==resultado_jugador){
-                    escribir.innerHTML="<h4>Correcto</h4>  <h5>¿Desea jugar de nuevo?</h5>";
-                    boton_empezar.style.display="block";
-                    boton_corregir.style.display="none";
+                    correcto=true;
                 }
                 else{
-                    escribir.innerHTML="<h4 id='letra_incorrecto'>Incorrecto</h4>  <h5>Vuelva a intentarlo</h5>";
+                    correcto=false;
                 }
             }
-            break;
+            break;       
     }
+        if(correcto==true){
+            escribir.innerHTML="<h4>Correcto</h4>  <h5>¿Desea jugar de nuevo?</h5>";
+            boton_empezar.style.display="block";
+            boton_corregir.style.display="none";
+            boton_porcentajes.style.dysplay="block";
+            porcentaje_aciertos[i]=1;//vamos a definir el 1 como que ha acertado
+            i++;
+        }
+        else{
+            escribir.innerHTML="<h4 id='letra_incorrecto'>Incorrecto</h4>  <h5>Vuelva a intentarlo</h5>";
+            porcentaje_aciertos[i]=0;//vamos a definir el 0 como que ha fallado
+            i++;
+        }
 }
 
+function porcentaje(){
+    var num_acertadas=0;
+    var porcentaje;
+    for(var cont=0;cont<porcentaje_aciertos.length;cont++){
+        num_acertadas=num_acertadas+porcentaje_aciertos[cont];
+    }
+    porcentaje=(num_acertadas/porcentaje_aciertos.length)*100;
+    document.getElementById("porcentaje").innerHTML="<h5>Su porcentaje de acierto es:</h5> <h4>"+porcentaje+"%</h4>"
+}
